f288e3fd0d94e0fa789cc96cdf4abba44ad80408
Giovanni Bruno <giovanni.bruno@soluzionifutura.it>
2019-03-07 09:48:24 +0100

    aggiunto room parser

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # python-game
+
+## cosa vogliamo fare:
+realizziamo un programma per generare avventure grafiche a terminale.
+
+## cose da fare:
+
+- room
+  - creare la classe
+  - creare il lettore dei file testuali *.room*
+  - gestione della visualizzazione della stanza
+
+- ui: gestione della visualizzazione di  tutte le info per il giocatore
+
+- oggetti con le loro caratteristiche
+
+- inventario
+
+- oggetti dell'inventario
+
+- player
--- main.py
+++ main.py
@@ -1,17 +1,44 @@
 class Entity:
-  def __init__(self, x, y, graphic):
+  def __init__(self, x, y, graphic, name):
     self.x = x
     self.y = y
     self.graphic = graphic
+    self.name = name
+
+class Wall(Entity):
+  def __init__(self, x, y):
+    Entity.__init__(self, x, y, "M", "muro")
+
+class Room:
+  def __init__(self, number):
+    self.number = number
+    file = open("./rooms/{}.room".format(number))
+    rows = file.readlines()
+    file.close()
+    self.h = len(rows)
+    self.w = len(rows[0]) -1
+    self.entities = []
+
+    for y in range(self.h):
+      for x in range(self.w):
+        if rows[y][x] == "#":
+          self.entities.append(Wall(x, y))
 
 
-e = Entity(5, 5, "X")
+  def draw(self):
+    for y in range(self.h):
+      for x in range(self.w):
+        for e in self.entities:
+          if e.x == x and e.y == y:
+            print("[{}]".format(e.graphic), end="")
+            break
+        else:
+          print("[ ]", end="")
+      print()
 
-for y in range(10):
-  for x in range(10):
-    if e.x == x and e.y == y:
-      print("[{}]".format(e.graphic), end="")
-    else:
-      print("[ ]", end="")
+r = Room(0)
+r.draw()
+'''
 
   print()
+'''
